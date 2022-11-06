3357b0bf9ff229ebc23e9e0dd5a7ca9a16a4596d
NurikN999 <nurkamalnurmukhamed@gmail.com>
2022-11-06 13:48:49 +0500

    fixed_conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_25_022500) do
+ActiveRecord::Schema.define(version: 2022_11_05_082800) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.integer "creator"
   end
 
   create_table "users", force: :cascade do |t|
